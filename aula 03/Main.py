13db0df1d04b075a9cb7ec3d9dc1a5e1490d5ff3
VieiraLuan <105254554+VieiraLuan@users.noreply.github.com>
2022-09-15 19:52:24 -0300

    .

---
+++ aula 03/Main.py
@@ -0,0 +1,43 @@
+resposta = "sim"
+texto = "Brasil"
+print("s" in resposta)
+print("n" not in resposta)
+print("b" in texto)
+print("B" in texto)
+
+#----------------------------------------------------------
+print("Esse programa exibirá seu nome completo. ")
+nome = input("Digite seu primeiro nome, por favor: ")
+sobrenome = input("Digite seu sobrenome, por favor: ")
+nome_completo = nome + " " + sobrenome
+print(nome_completo)
+
+
+
+#atribuição da soma de dois números inteiros
+resultado = 2 + 3
+print("Resultado:", resultado)
+#atribuição de um texto (string)
+nome = "Pedro"
+print("Nome: ", nome)
+nome2 = "Maria"
+#saída f"string" com a variável entre as chaves
+print(f"Olá, {nome2}. Tudo bem?")
+#somando dois ao valor anterior da variável
+resultado += 2
+print("Resultado:", resultado)
+
+
+A = 1 == 2
+B = 2 > 3
+print(A)
+print(B)
+#Operações
+print(A and B)
+print(A or B)
+print(not A)
+print(not B)
+print(8>=5 and 8<=10)
+
+
+
